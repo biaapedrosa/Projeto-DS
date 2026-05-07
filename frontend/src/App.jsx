3893608240ac7944710888e9e03057e61fbe9fe3
beatrizpandolfi <beatrizpmaroja@gmail.com>
2026-05-07 18:24:04 -0300

    feat: implementação da SearchBar e DatePicker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import NavBar from './components/NavBar/NavBar';
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic' // Isso faz o Vite injetar o React automaticamente em todos os arquivos
+  })],
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.[jt]sx?$/,
