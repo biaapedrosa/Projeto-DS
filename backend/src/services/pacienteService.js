79a9a93dc827979e8351374a8b58e37c8fa49cdc
Caio Agrelli <caiooagrelli@hotmail.com>
2026-06-03 18:07:00 -0300

    feat(paciente): adiciona criacao de paciente pelo nutricionista

--- backend/src/services/pacienteService.js
+++ backend/src/services/pacienteService.js
@@ -1,5 +1,13 @@
+const bcrypt = require('bcryptjs');
 const pacienteRepository = require('../repositories/pacienteRepository');
 const planoRepository = require('../repositories/planoRepository');
+
+const create = async ({ nome, email, senha, sexo, data_nascimento, idade, telefone_whatsapp, ocupacao, vinculo_ufpe, objetivo }) => {
+  const existente = await pacienteRepository.findByEmail(email);
+  if (existente) throw new Error('Email já cadastrado!');
+  const senhaHash = await bcrypt.hash(senha, 10);
+  return pacienteRepository.create({ nome, email, senha: senhaHash, sexo, data_nascimento, idade, telefone_whatsapp, ocupacao, vinculo_ufpe, objetivo });
+};
 
 const getById = async (id) => {
   const paciente = await pacienteRepository.findById(id);
@@ -24,4 +32,4 @@ const remove = async (id) => {
   if (!paciente) throw new Error('Paciente não encontrado!');
   await pacienteRepository.remove(id);
 }
-module.exports = { getById, update, getPlanos, getAll, remove };
+module.exports = { create, getById, update, getPlanos, getAll, remove };
